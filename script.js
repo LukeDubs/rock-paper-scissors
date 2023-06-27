3c7946d628af3b1e5f11814ac0a182f39e654af1
Luke Walden <luke@pop-os.localdomain>
2023-06-27 11:44:44 +0100

    Add to script.js
    first two functions working third needs work

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 console.log('hello, world');
+
+const getComputerChoice = () => {
+    let randomNum = Math.floor((Math.random() * 3) + 1);
+
+    if (randomNum === 1) {
+        return 'Rock'; 
+    } else if (randomNum === 2) {
+        return 'Paper';
+    } else {
+        return 'Scissors';
+    }
+}
+
+const playRound = (playerSelection, computerSelection) => {
+    let ps = playerSelection.toLowerCase();;
+    let cs = computerSelection.toLowerCase();
+    
+    if ((ps === 'rock' && cs === 'paper') || (ps === 'paper' && cs === 'scissors')
+    || (ps === 'scissors' && cs === 'rock')) {
+        return 'You lose! ' + computerSelection + ' beats ' + playerSelection;
+    } else if ((ps === 'paper' && cs === 'rock') || (ps === 'scissors' && cs === 'paper')
+    || (ps === 'rock' && cs === 'scissors')) {
+        return 'You win! ' + playerSelection + ' beats ' + computerSelection;
+    } else {
+        return "It's a draw, you both picked " + computerSelection;
+    }
+}
+
+const game = () => {
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt('Rock, Paper or Scissors');
+        const computerSelection = getComputerChoice(); 
+        let score = 0;  
+        
+        playRound(playerSelection, computerSelection);
+
+        if 
+        
+    }
+    
+}
+ 
+console.log(game());
